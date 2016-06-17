982c314b36e4ae1a8330a7c3d28c3e000a0fcf15
samgreen <greensamuelm@gmail.com>
2016-06-17 11:19:39 -0700

    Remove drop events test.

--- HelloMixpanel/HelloMixpanelTests/HelloMixpanelTests.m
+++ HelloMixpanel/HelloMixpanelTests/HelloMixpanelTests.m
@@ -861,19 +861,6 @@
     XCTAssertEqualObjects(r[@"$set"][@"$transactions"], @[]);
 }
 
-//- (void)testDropEvents
-//{
-//    for (NSInteger i = 0; i < 5005; i++) {
-//        [self.mixpanel track:@"rapid_event" properties:@{@"i": @(i)}];
-//    }
-//    [self waitForSerialQueue];
-//    XCTAssertTrue([self.mixpanel.eventsQueue count] == 5000);
-//    NSDictionary *e = self.mixpanel.eventsQueue[0];
-//    XCTAssertEqualObjects(e[@"properties"][@"i"], @(5));
-//    e = [self.mixpanel.eventsQueue lastObject];
-//    XCTAssertEqualObjects(e[@"properties"][@"i"], @(5004));
-//}
-
 - (void)testDropUnidentifiedPeopleRecords
 {
     for (NSInteger i = 0; i < 505; i++) {
